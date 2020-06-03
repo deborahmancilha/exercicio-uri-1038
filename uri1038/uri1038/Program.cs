155ff1582b059feebd9e47fcce0ea0a4d6675b90
deborahmancilha@outlook.com <deborahmancilha@outlook.com>
2020-06-03 16:10:07 -0300

    Versão 0.1

---
+++ uri1038/uri1038/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo, quantidade;
+            double c1, c2, c3, c4, c5;
+
+            String[] vet;
+            vet = Console.ReadLine().Split(' ');
+            codigo = int.Parse(vet[0]);
+            quantidade = int.Parse(vet[1]);
+
+            c1 = (double)quantidade * 4.00;
+            c2 = (double)quantidade * 4.50;
+            c3 = (double)quantidade * 5.00;
+            c4 = (double)quantidade * 2.00;
+            c5 = (double)quantidade * 1.50;
+
+
+            if (codigo == 1)
+            {
+                Console.WriteLine("Total: R$ " + c1.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            else
+            {
+                if (codigo == 2)
+                {
+                    Console.WriteLine("Total: R$ " + c2.ToString("F2", CultureInfo.InvariantCulture));
+
+                }
+                else
+                {
+                    if (codigo == 3)
+                    {
+                        Console.WriteLine("Total: R$ " + c3.ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                    else
+                    {
+                        if (codigo == 4)
+                        {
+                            Console.WriteLine("Total: R$ " + c4.ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Total: R$ " + c5.ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
